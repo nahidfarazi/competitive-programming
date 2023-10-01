ddac24c57d0741a0651d034a96eebd6fbbfe5c47
Nahid Farazi <artistnahid24@gmail.com>
2023-10-01 22:13:31 +0600

    Variable and Data Types

---
+++ C/Module-1/varData.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+int main()
+{
+    /* integer practice */
+    int myAge;  myAge = 20;
+
+    int myDate_of_Birth, myClass;   myDate_of_Birth = 29;   myClass = 12;
+    int myId = 6522;
+
+    printf("%d\n",myAge);
+    printf("%d\n", myDate_of_Birth);
+    printf("%d\n",myClass);
+    printf("%d\n",myId);
+
+    /* Float practice */
+    float myResult = 4.11;
+    
+    printf("%f\n",myResult);
+    printf("%0.2f\n",myResult);
+
+    /* Char practice */
+    char myName = 'N';
+    printf("%c\n",myName);
+
+    /* Boolean practice */
+    
+     return 0;
+}
