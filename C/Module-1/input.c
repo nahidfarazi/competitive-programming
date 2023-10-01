1b103db5611d97f5d0d10eead2f2964d8d2decc8
Nahid Farazi <artistnahid24@gmail.com>
2023-10-01 22:46:57 +0600

    input

---
+++ C/Module-1/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    /*Integer and Float*/
+    /*int myAge; float myResult;
+    char myName;
+    scanf("%d %f %c",&myAge, &myResult, &myName);
+    printf("%d\n", myAge);
+    printf("%0.2f\n", myResult);
+    printf("%c\n",myName);*/
+
+    int a, b;
+    scanf("%d %d",&a, &b);
+    printf("%d%% %d%% \n",a, b);
+    return 0;
+}
