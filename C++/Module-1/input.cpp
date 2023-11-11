d793f72f38e4ac6156f7e90cefd29cd2ce38fba6
Nahid Farazi <artistnahid24@gmail.com>
2023-11-11 23:38:03 +0600

    input,output,ternary,switch case done

---
+++ C++/Module-1/input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a << endl;
+    char c;
+    cin >> c;
+    cout << c << endl;
+    cout << int(c) << endl;
+
+    return 0;
+}
